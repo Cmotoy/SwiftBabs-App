e270a801e570ad29d3310f751455a05518fe312b
Oluwabunmi Omotoyinbo <cmotoy@outlook.com>
2020-07-05 12:49:35 +0100

    added authentication db context class

--- src/SwiftBabsApp.Data/DatabaseContexts/AuthenticationDBContext/AuthenticationDbContext.cs
+++ src/SwiftBabsApp.Data/DatabaseContexts/AuthenticationDBContext/AuthenticationDbContext.cs
@@ -1,6 +1,15 @@
-namespace SwiftBabsApp.Data.DatabaseContext.AuthenticationDBContext{
-    public class AuthenticationDBContext
+using SwiftBabsApp.Data.Entities;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace SwiftBabsApp.Data.DatabaseContext.AuthenticationDBContext
+{
+
+     public class AuthenticationDbContext : IdentityDbContext<ApplicationUser>
     {
-        
+        public AuthenticationDbContext(DbContextOptions<AuthenticationDbContext> options)
+        : base(options)
+        {
+        }
     }
 }
